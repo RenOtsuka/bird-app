3fcab5c5cff25185d5e1aee7b35a4628cd2d887a
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-11-23 11:50:13 -0600

    Add show action

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -4,5 +4,12 @@ class BirdsController < ApplicationController
     birds = Bird.all
     render json: birds
   end
-  
+
+  def show
+    bird = Bird.find(params[:id])
+    render json: bird
+  rescue ActiveRecord::RecordNotFound
+    render json: "Bird not found", status: :not_found
+  end
+
 end
